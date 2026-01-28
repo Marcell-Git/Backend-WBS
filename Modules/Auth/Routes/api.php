6706315ed1f8be318f24f081b53627fd454b43b0
Marcell-Git <220711662@students.uajy.ac.id>
2026-01-28 10:12:17 +0700

    add throttle

--- Modules/Auth/Routes/api.php
+++ Modules/Auth/Routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Auth\Http\Controllers\AuthController;
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->middleware('throttle:5,1');
 
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
